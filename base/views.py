81b818b0303e98aa8885028c69cf0d45ae7968e0
Suraj Singh <absurajsinghchamiyal@gmail.com>
2023-02-11 02:25:44 +0530

    rendering using templates

--- base/views.py
+++ base/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'home.html')
+
+
 def room(request):
-    return HttpResponse("Room")
+    return render(request, 'room.html')
