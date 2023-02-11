8fba9951eb9874c1d8461ed59a7fe6be36803a2d
Suraj Singh <absurajsinghchamiyal@gmail.com>
2023-02-11 16:51:54 +0530

    migrating models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    # host=
+    # topic=
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants=
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def _str__(self):
+        return self.name
+
+
+# Make migrations
+# python manage.py makemigrations
+# python manage.py migrate
