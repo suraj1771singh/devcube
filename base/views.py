d9eaa4a8f131c52b13718b5d7b7ede37e2cd46b7
Suraj Singh <absurajsinghchamiyal@gmail.com>
2023-02-13 00:55:44 +0530

    delete comments functionality added

--- base/views.py
+++ base/views.py
@@ -73,16 +73,18 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     # return set of all msgs that belong to the room
     room_messages = room.message_set.all().order_by('-created')
-
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -127,3 +129,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    msg = Message.objects.get(id=pk)
+
+    if request.user != msg.user:  # different user is not allowed to update room
+        return HttpResponse('You are not allowed here !')
+
+    if (request.method == 'POST'):
+        msg.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': msg})
